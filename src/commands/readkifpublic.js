ded5eade83d4d2f30ace8e948055061b16934597
taddeig <gilles.taddei@sacem.fr>
2022-10-24 10:41:07 +0200

    ajout du username dans le public read

--- src/commands/readkifpublic.js
+++ src/commands/readkifpublic.js
@@ -3,7 +3,7 @@ const pgController = require('../pgController');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('lireunkifpublic')
-        .setDescription('Permet de lire un kif !'),
+        .setDescription('Permet de lire un kif publiquement !'),
     async execute(interaction) {
         pgController.getRandomKif(interaction.user.id, interaction, false);
     },
--- src/pgController.js
+++ src/pgController.js
@@ -22,11 +22,13 @@ const getRandomKif = (userId, interaction, ephemeral) => {
         "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
     const query = 'SELECT "username", "kif", date_part(\'day\', "dateKif"::date) as day_out, date_part(\'month\', "dateKif"::date) as month_out, date_part(\'year\', "dateKif"::date) as year_out FROM kif WHERE "userId" like $1 ORDER BY random() LIMIT 1;';
     client.query(query, [userId])
-        .then(res =>{
-            const {day_out, month_out, year_out, kif} = res.rows[0];
-            let ret = `Le ${day_out} ${monthNames[month_out - 1]} ${year_out}, tu as eu ce kif : ${kif}`;
+        .then(res => {
+            const {day_out, month_out, year_out, kif, username} = res.rows[0];
+            let ret = ephemeral ?
+                `Le ${day_out} ${monthNames[month_out - 1]} ${year_out}, tu as eu ce kif : ${kif}`
+                : `Le ${day_out} ${monthNames[month_out - 1]} ${year_out}, ${username} a eu ce kif : ${kif}`;
             interaction.reply({content: ret, ephemeral: ephemeral});
-    })
+        })
 };
 
 const getUserIds = async () => {
